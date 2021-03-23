87fc3077c6b50aefe30e24a33844850d702e3033
cnhhoang850 <cnhhoang850@gmail.com>
2021-03-23 11:43:28 +0700

    added text change

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const titleHead = document.querySelector("#titleHeader");
 const titleText = document.querySelector("#titleText");
 
 const titleTexts = {
-  Home: `Hi, I'm Hoang. I develop art to app on the web
-         that focus on bringing people out of the web.
-         This is my portfolio + blog.`,
+  Home: `Hi, I'm Hoang. I develop art to app on the web that focus on bringing people out of the web. This is my portfolio + blog.`,
   Web: `I approach web experience development through systems thinking, user centered goals, and designing just enough.`,
   Photos: `The goal of my photos are first themselves. My photography straddles on the line of power between the presenter and the represented.`,
-  Reading: `This is were I catalog what I read or am reading.
-            Drop a recommendation if you see anything related.`,
+  Reading: `This is were I catalog what I read or am reading. Drop a recommendation if you see anything related.`,
   Listening: `Who doesn't love music?`,
 }
 
